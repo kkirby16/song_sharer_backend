a95832ee050fa4c3356affa115918d052c998205
kkirby16 <kkirby16@my.whitworth.edu>
2021-06-14 23:11:47 -0700

    add song routes and generated controller

---
+++ app/controllers/api/v1/songs_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::SongsController < ApplicationController
+end
+
+#we want to be explicit about how our controller is declared here at the top to reflect our namespaced routes.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do #gives us version control for multiple apis we might want to have
+      resources :songs, only: [:index] #since building vertically only going to focus on index for now.
+    end
+  end
 end
