573a09882d98d6f005b110844b290110c22b429c
kkirby16 <kkirby16@my.whitworth.edu>
2021-06-16 21:37:17 -0700

    add null: false to foreign key reference

--- db/migrate/20210615034832_add_genre_to_songs.rb
+++ db/migrate/20210615034832_add_genre_to_songs.rb
@@ -1,5 +1,5 @@
 class AddGenreToSongs < ActiveRecord::Migration[6.0]
   def change
-    add_reference :songs, :genre, foreign_key: true
+    add_reference :songs, :genre, null: false, foreign_key: true
   end
 end
