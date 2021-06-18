0d255c3d3b99ca41c9786df7fdbcc08b398613ae
kkirby16 <kkirby16@my.whitworth.edu>
2021-06-17 22:10:26 -0700

    uncomment code in cors.rb that sets up rack cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "*" #this is where we list the respective origins we want to allow like our frontends website url on netlify
+
+    resource "*", #can specify which resources we want to allow requests to be made of
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
