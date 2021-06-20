bf39e9a4828490146f6b0c81469eedb8c7a96c0d
kkirby16 <kkirby16@my.whitworth.edu>
2021-06-20 01:31:03 -0700

    changed create action to make sure I'm rendering json with SongSerializer class

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -13,18 +13,18 @@ class Api::V1::SongsController < ApplicationController
   def create
     song = Song.new(song_params)
     if song.save
-      render json: song, status: :accepted
+      render json: SongSerializer.new(song), status: :accepted
     else
       render json: { errors: song.errors.full_messages }, status: :unprocessible_entity
       #we can send these error messages to the frontend to display them to the user.
     end
   end
-end
 
-private
+  private
 
-def song_params
-  params.require(:song).permit(:name, :artist, :album, :song_url, :submitted_by, :genre_id)
+  def song_params
+    params.require(:song).permit(:name, :artist, :album, :song_url, :submitted_by, :genre_id)
+  end
 end
 
 #we want to be explicit about how our controller is declared here at the top to reflect our namespaced routes.
