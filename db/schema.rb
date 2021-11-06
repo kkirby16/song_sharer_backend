84bb8daaed3ee8fffca93decd0caeae176af0d82
kkirby16 <kkirby16@my.whitworth.edu>
2021-11-05 18:04:30 -0700

    remigrate the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_034832) do
+ActiveRecord::Schema.define(version: 2021_11_02_032648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_034832) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "genre_id", null: false
+    t.integer "likes", default: 0
     t.index ["genre_id"], name: "index_songs_on_genre_id"
   end
 
