f8ea586be94db39fe10b329b42baaf8d1e9efeeb
kkirby16 <kkirby16@my.whitworth.edu>
2022-01-02 21:54:09 -0800

    change up routes to have resources likes be in charge of creating/deleting likes instead of it being nested under songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do #gives us version control for multiple apis we might want to have
-      resources :songs, only: [:index, :create, :show] do
-        member do
-          patch "/api/v1/add_like_to_song/:id", to: "songs#update_for_adding_like_to_song"     #since building vertically only going to focus on index for now.
-          patch "/api/v1/delete_like_from_song/:id", to: "songs#update_for_deleting_like_from_song"
-        end
-      end
+      resources :songs, only: [:index, :create, :show] #since building vertically only going to focus on index for now.
       resources :genres, only: [:index]
+      resources :likes
     end
   end
 end
