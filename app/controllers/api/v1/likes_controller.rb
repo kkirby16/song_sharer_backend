0488a583b099cce551bddc09a1031b8ae3ba0384
kkirby16 <kkirby16@my.whitworth.edu>
2022-01-02 21:40:57 -0800

    Revert "create likes controller and run migration to create likes table and to remove likes attribute from songs table"
    This reverts commit 61451706f6da676c60f815fa9c0a868b279d3645.

--- app/controllers/api/v1/likes_controller.rb
+++
@@ -1,2 +0,0 @@
-class Api::V1::LikesController < ApplicationController
-end
--- db/migrate/20220103050236_remove_likes_from_songs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLikesFromSongs < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :songs, :likes, :integer
-  end
-end
