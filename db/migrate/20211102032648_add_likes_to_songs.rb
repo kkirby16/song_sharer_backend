e95535e95a5e7b39214baa989bb8bd68472a837a
kkirby16 <kkirby16@my.whitworth.edu>
2022-02-17 21:30:28 -0800

    change default value for likes on a song back to 0

--- db/migrate/20211102032648_add_likes_to_songs.rb
+++ db/migrate/20211102032648_add_likes_to_songs.rb
@@ -1,5 +1,5 @@
 class AddLikesToSongs < ActiveRecord::Migration[6.0]
   def change
-    add_column :songs, :likes, :integer, default: 1
+    add_column :songs, :likes, :integer, default: 0
   end
 end
