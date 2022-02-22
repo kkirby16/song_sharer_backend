3deaabd681c4610e14ef1f2b12f162f36d973b86
kkirby16 <kkirby16@my.whitworth.edu>
2022-02-21 23:54:39 -0800

    change comment

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -3,6 +3,6 @@ class SongSerializer
   attributes :name, :artist, :album, :song_url, :submitted_by, :genre_id, :genre, :likes
 end
 
-#json serialier is good for scalability as it is nesting our attributes and data in a way that is very descriptive and makes it easier
+#json serializer is good for scalability as it is nesting our attributes and data in a way that is very descriptive and makes it easier
 #with fast json api serializer we can control what gets rendered in our json data and we can also create and display assocations in our data.
 #serializer helps us display our associated data in our json so on the frontend we can display our assocations.
